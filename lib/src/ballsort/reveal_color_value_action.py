b7f51204011e2fb582e3bacffd87db53480eff10
Anders Heed <aheed88888@gmail.com>
2023-10-23 16:03:53 +0200

    correct ball-in-claw updates

--- lib/src/ballsort/reveal_color_value_action.py
+++ lib/src/ballsort/reveal_color_value_action.py
@@ -7,11 +7,18 @@ class RevealColorValueAction:
 
     pos: StatePosition
 
+    def __reveal_ball(self, src_ball: StateBall, target_ball: StateBall | None) -> StateBall | None:
+        if target_ball is None:
+            return None
+        intermediate_ball = replace(target_ball, value_visible = True if target_ball.color==src_ball.color else target_ball.value_visible)
+        ret = replace(intermediate_ball, label = f"{intermediate_ball.value}" if intermediate_ball.value_visible else intermediate_ball.label)
+        return ret
+
     def on_ball_dropped(self, state: StateModel, ball: StateBall) -> tuple[StateModel, bool]:
         if ball.pos != self.pos:
             return state, False
         
-        balls = [replace(b, value_visible = True if b.color==ball.color else b.value_visible) for b in state.balls]
-        balls = [replace(b, label = f"{b.value}" if b.value_visible else b.label) for b in balls]
+        balls = [self.__reveal_ball(src_ball=ball, target_ball=b) for b in state.balls]
+        claws = [replace(claw, ball=self.__reveal_ball(src_ball=ball, target_ball=claw.ball)) for claw in state.claws]
 
-        return replace(state, balls=balls), True
+        return replace(state, balls=balls, claws=claws), True
