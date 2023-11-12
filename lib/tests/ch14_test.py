a9066bdbf98947ba619f28a3889e60d0bc4b9c33
Anders Heed <aheed88888@gmail.com>
2023-11-12 23:44:46 +0100

    ch14 wip

--- lib/tests/ch14_test.py
+++ lib/tests/ch14_test.py
@@ -1,10 +1,10 @@
 import asyncio
 from dataclasses import replace
 import sys
-
-from test_utils import move_ball, move_ball_by_column
 sys.path.append("../src/ballsort")
 
+from ball_control import BallControl
+from test_utils import move_ball_by_column
 from control_factory import get_control_sim
 from ch14_scenario import Ch14Scenario
 from state_update_model import StateBall, StatePosition
@@ -41,23 +41,33 @@ def goal_state():
     state = replace(sc.get_initial_state(), balls=balls)
     assert sc.is_in_goal_state(state) == True
 
+async def make_moves_single_claw(bc: BallControl,  moves: list[tuple[int, int]]):
+    for move in moves:
+        src_x, dest_x = move
+        await move_ball_by_column(bc=bc, src_x=src_x, dest_x=dest_x)
 
-async def example_solution_single_claw():
-    bc = get_control_sim(0)
-    await bc.set_scenario(Ch14Scenario())
-
-    async def sort_column(x: int):
+def get_winning_sequence() -> list[tuple[int, int]]:
+    moves: list[tuple[int, int]] = []
+    def sort_column(x: int):
 
         def get_temporary_column(x: int, i: int) -> int:
             return i if i < x else i+1
         
         for i in range(4):
-            await move_ball_by_column(bc=bc, src_x=x, dest_x=get_temporary_column(x=x, i=i))
+            moves.append((x, get_temporary_column(x=x, i=i)))
         for i in range(4):
-            await move_ball_by_column(bc=bc, src_x=get_temporary_column(x=x, i=i), dest_x=x)
+            moves.append((get_temporary_column(x=x, i=i), x))
 
     for i in range(1, 5):
-        await sort_column(x=i)
+        sort_column(x=i)
+
+    return moves
+
+async def example_solution_single_claw():
+    bc = get_control_sim(0)
+    await bc.set_scenario(Ch14Scenario())
+
+    await make_moves_single_claw(bc=bc, moves=get_winning_sequence())
 
     print(f"virtual time elapsed: {bc.get_state().elapsed:0.3f} seconds")
 
